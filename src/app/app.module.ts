d5f9a10b365b2454cc2708eeb1d4730e76896b7a
Alex Ilin <alexander.ilin@autodesk.com>
2019-09-24 19:08:52 +0300

    angular ngrx app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { AppComponent } from '@app/app.component';
+import { AuthService } from '@app/services/auth.service';
+import { ApiService } from '@app/services/api.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument(),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AuthService, ApiService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '@env/environment';
+import { AuthType, AuthDTO } from '@app/models/auth';
+import { Observable } from 'rxjs';
+import { User } from '@app/models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private api: string = environment.api_server + '/';
+
+  constructor(private http: HttpClient) {}
+
+  private auth(authType: AuthType, data: AuthDTO): Observable<User> {
+    return this.http.post<User>(`${this.api}/${authType}`, data);
+  }
+
+  login(data: AuthDTO): Observable<User> {
+    return this.auth('login', data);
+  }
+
+  register(data: AuthDTO): Observable<User> {
+    return this.auth('register', data);
+  }
+
+  get token() {
+    return localStorage.getItem('token');
+  }
+
+  set token(val: string) {
+    if (val) {
+      localStorage.setItem('token', val);
+    } else {
+      localStorage.removeItem('token');
+    }
+  }
+}
